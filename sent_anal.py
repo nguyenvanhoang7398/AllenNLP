fa7a26b98c3c886218ac59e201aa75248100fffb
nguyenvanhoang7398 <nguyenvanhoang7398@gmail.com>
2018-12-08 17:13:15 +0800

    Add validation

--- sent_anal.py
+++ sent_anal.py
@@ -1,22 +1,22 @@
-
+from allennlp.common import JsonDict
+from allennlp.data import DatasetReader, Instance
 from allennlp.data.dataset_readers.stanford_sentiment_tree_bank import StanfordSentimentTreeBankDatasetReader
 from allennlp.data.iterators.bucket_iterator import BucketIterator
 from allennlp.data.vocabulary import Vocabulary
-
 from allennlp.models.model import Model
-
 from allennlp.modules.seq2vec_encoders.pytorch_seq2vec_wrapper import PytorchSeq2VecWrapper
 from allennlp.modules.seq2vec_encoders.seq2vec_encoder import Seq2VecEncoder
 from allennlp.modules.text_field_embedders import BasicTextFieldEmbedder, TextFieldEmbedder
 from allennlp.modules.token_embedders.embedding import Embedding
-
 from allennlp.nn.util import get_text_field_mask
-
+from allennlp.predictors import Predictor
 from allennlp.training.metrics.categorical_accuracy import CategoricalAccuracy
 from allennlp.training.trainer import Trainer
 
+import numpy as np
+from overrides import overrides
 import torch
-from typing import Dict
+from typing import Dict, List
 
 EMBEDDING_DIM = 50
 HIDDEN_DIM = 32
@@ -74,6 +74,27 @@ trainer = Trainer(model=model,
                   train_dataset=train_dataset,
                   validation_dataset=dev_dataset,
                   patience=10,
-                  num_epochs=20)
-
+                  num_epochs=10)
 trainer.train()
+
+@Predictor.register("sentence_classifier_predictor")
+class SentenceClassifierPredictor(Predictor):
+    def __init__(self,
+                 model: Model,
+                 dataset_reader: DatasetReader) -> None:
+        super().__init__(model, dataset_reader)
+
+    def predict(self,
+                tokens: List[str]) -> JsonDict:
+        return self.predict_json({"tokens": tokens})
+
+    @overrides
+    def _json_to_instance(self,
+                          json_dict: JsonDict) -> Instance:
+        tokens = json_dict["tokens"]
+        return self._dataset_reader.text_to_instance(tokens)
+
+tokens = ['This', 'is', 'best', 'movie', 'ever', '!']
+predictor = SentenceClassifierPredictor(model, dataset_reader=reader)
+logits = predictor.predict(tokens)['logits']
+label_id = np.argmax(logits)
